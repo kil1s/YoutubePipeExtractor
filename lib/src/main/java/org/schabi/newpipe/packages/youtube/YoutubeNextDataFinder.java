14e18a13edccbde5ebd483e835ae9a96bf3eaced
Florian L. Steenbuck <admin@kil.ls>
2025-03-24 22:04:23 +0100

    #notworking #anti-static / Package / (copy) and (add) finder, requester and validator logic

---
+++ lib/src/main/java/org/schabi/newpipe/packages/youtube/YoutubeNextDataFinder.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.schabi.newpipe.packages.youtube;
+
+/**
+ *
+ * @author Florian
+ */
+public class YoutubeNextDataFinder {
+    
+}
